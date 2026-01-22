3be0663eaba974f875fed11a154a5d0d936240df
Raghu <raghuramroyya2022@gmail.com>
2026-01-22 15:42:52 +0530

    final Update to CORS

--- src/main/java/com/credresolveAssignment/expense_sharing/config/CorsConfig.java
+++ src/main/java/com/credresolveAssignment/expense_sharing/config/CorsConfig.java
@@ -10,7 +10,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173")
+                .allowedOrigins(
+                "http://localhost:5173",
+                "https://expense-sharing-frontend.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
